cd0a26335ae0db7a3269b80240ca2bd8ddcc0944
Oliver Tran <olivertransf@gmail.com>
2025-11-20 20:57:38 -0800

    Initial Commit

---
+++ Claveo/ClaveoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaveoApp.swift
+//  Claveo
+//
+//  Created by Oliver Tran on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaveoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
